e7f2dc68f942bf5a77e3d598e546b414c43b2f05
DESKTOP-P678S41\wmh <dnjsalsgh123@gmail.com>
2023-06-07 20:30:13 +0900

    ADD(:zap:): Limit using api -v3

--- nodeBirdApi/middlwares/index.js
+++ nodeBirdApi/middlwares/index.js
@@ -1,54 +1,61 @@
 const jwt = require('jsonwebtoken');
 const rateLimit = require('express-rate-limit');
+const {User} = require('../models')
+exports.isLoggedIn = (req, res, next) => {
 
-exports.isLoggedIn = (req,res,next) => {
-
-    if(req.isAuthenticated()){ // 패스포트 통해서 로그인 했니?
+    if (req.isAuthenticated()) { // 패스포트 통해서 로그인 했니?
         next();
-    } else{
+    } else {
         res.status(403).send('로그인 필요');
     }
 }
 
 exports.isNotLoggedIn = (req, res, next) => {
-    if(!req.isAuthenticated()){
+    if (!req.isAuthenticated()) {
         next();
-    } else{
+    } else {
         const message = encodeURIComponent('로그인한 상태입니다.')
         res.redirect(`/?error=${message}`);
     }
 }
 
-exports.verifyToken = (req, res, next) =>{
-    try{
+exports.verifyToken = (req, res, next) => {
+    try {
         res.locals.decoded = jwt.verify(req.headers.authorization, process.env.JWT_SECRET)
         return next();
-    }catch(error){
-        if(error.name === 'TokenExpiredError'){
+    } catch (error) {
+        if (error.name === 'TokenExpiredError') {
             return res.status(419).json({
-                code:419,
+                code: 419,
                 message: '토큰이 만료되었습니다.'
             })
         }
         return res.status(401).json({
-            code:401,
+            code: 401,
             message: '유효하지 않은 토큰입니다.'
         })
     }
 }
 
-exports.apiLimiter = rateLimit({
-    windowMs: 60 * 1000,
-    max: 10,
-    handler(req,res) {
-        res.status(this.statusCode).json({
-            code: this.statusCode,
-            message: '1분에 한 번만 요청할 수 있습니다.'
-        })
-    }
-})
+exports.apiLimiter = async (req, res, next) => {
 
-exports.deprecated = (req, res) =>{
+    let user;
+    if (res.locals.decoded.id) {
+        user = await User.findOne({where: {id: res.locals.decoded.id}});
+    }
+    rateLimit({
+        windowMs: 60 * 1000,
+        max: user?.type === 'premium' ? 1000 : 10,
+        handler(req, res) {
+            res.status(this.statusCode).json({
+                code: this.statusCode,
+                message: '1분에 한 번만 요청할 수 있습니다.'
+            })
+        }
+    })(req, res, next);
+}
+
+exports.deprecated = (req, res) => {
     res.status(410).json({
         code: 410,
         message: '새로운 버전 나왔습니다. 새로운 버전을 사용해주세요'
--- nodeBirdApi/routes/v2.js
+++ nodeBirdApi/routes/v2.js
@@ -1,15 +1,15 @@
 const express = require('express')
 const {verifyToken, apiLimiter} = require("../middlwares");
-const { createToken, tokenTest, getMyPosts, getPostsByHashtag} = require('../controllers/v2')
+const {createToken, tokenTest, getMyPosts, getPostsByHashtag} = require('../controllers/v2')
 
 const router = express.Router();
 
 
 // /v2/token
 router.post('/token', apiLimiter, createToken); //req.body.clientSecret 에 토큰 주입
-router.get('/test',apiLimiter, verifyToken, tokenTest);
+router.get('/test', verifyToken, apiLimiter, tokenTest);
 
-router.get('/posts/my',apiLimiter,verifyToken, getMyPosts);
-router.get('/posts/hashtag/:title',apiLimiter, verifyToken, getPostsByHashtag);
+router.get('/posts/my', verifyToken, apiLimiter, getMyPosts);
+router.get('/posts/hashtag/:title', verifyToken, apiLimiter, getPostsByHashtag);
 
 module.exports = router;
